962d1b06f4cf428d6d23095bf43a62823037ec22
Mathildergnr <mthldregnier@gmail.com>
2023-11-22 10:00:14 +0200

    First commit

---
+++ src/main/java/net/efrei/demo/ProjetM2Application.java
@@ -0,0 +1,13 @@
+package net.efrei.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetM2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetM2Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/efrei/demo/ProjetM2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.efrei.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetM2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
