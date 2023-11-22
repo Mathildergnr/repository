d3c7361c2613fd71cab20232062d1f824982a7cf
Mathildergnr <mthldregnier@gmail.com>
2023-11-22 11:51:14 +0200

    #1 add WS

---
+++ src/main/java/net/efrei/demo/web/rest/ItemRessource.java
@@ -0,0 +1,16 @@
+package net.efrei.demo.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+
+public class ItemRessource {
+    @GetMapping("/items")
+    public String test(){
+        return "Hello World !";
+    }
+}
